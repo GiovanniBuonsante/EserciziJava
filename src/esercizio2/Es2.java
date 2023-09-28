701fba9a86c937eee0bcb2f1976f1961b9389015
GiovanniBuonsante <buonsanteg@gmail.com>
2023-09-28 11:36:24 +0200

    Aggiornamento

--- src/esercizio2/Es2.java
+++ src/esercizio2/Es2.java
@@ -1,4 +1,17 @@
 package esercizio2;
 
+import java.util.Scanner;
+
 public class Es2 {
+    public static void main(String[] args){
+        int number;
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter one integer: ");
+        number = input.nextInt();
+        if(number % 2 == 0){
+            System.out.println("The number is even");
+        }else{
+            System.out.println("The number is odd");
+        }
+    }
 }
