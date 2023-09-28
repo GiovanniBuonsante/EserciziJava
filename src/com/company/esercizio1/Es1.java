deb310e1afd039cf7ca55898a81f243ac938fd71
Buonsante <buonsantev@gmail.com>
2023-09-28 15:49:29 +0200

    Try/Catch example

--- src/com/company/esercizio1/Es1.java
+++ src/com/company/esercizio1/Es1.java
@@ -7,11 +7,16 @@ public class Es1 {
     public static void main(String[] args) {
 	// write your code here
         Scanner input = new Scanner(System.in);
-        int numberInput,numberMax, numberMin;
+        int numberInput = 0, numberMax, numberMin;
 
         //try to move all the input in the for starting from 0
         System.out.println("Enter the 1 number");
-        numberInput = input.nextInt(); //remember to handle the not valid input such as character.
+        try {
+            numberInput = input.nextInt(); //remember to handle the not valid input such as character.
+        } catch (Exception e) {
+            //Excpetion handled - Do something
+        }
+
         numberMax = numberInput;
         numberMin = numberInput;
         for(int i = 1;i < 5; i++){
