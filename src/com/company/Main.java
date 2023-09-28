fc77f559bb73eb444aaba338365a395c94bc27e1
GiovanniBuonsante <buonsanteg@gmail.com>
2023-09-28 11:25:14 +0200

    primo esercizio

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,27 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+        Scanner input = new Scanner(System.in);
+        int numberInput,numberMax, numberMin;
+        System.out.println("Enter the 1 number");
+        numberInput = input.nextInt();
+        numberMax = numberInput;
+        numberMin = numberInput;
+        for(int i = 1;i < 5; i++){
+            System.out.printf("Enter the %d number",i+1);
+            numberInput = input.nextInt();
+            if(numberInput > numberMax){
+                numberMax = numberInput;
+            }
+            if(numberInput < numberMin){
+                numberMin = numberInput;
+            }
+        }
+        System.out.printf("The number min is %d\nThe number max is: %d",numberMin,numberMax);
     }
 }
