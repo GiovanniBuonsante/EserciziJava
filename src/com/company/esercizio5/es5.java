92f6733261dec78cddee27d194a66040eb492b5e
GiovanniBuonsante <buonsanteg@gmail.com>
2023-09-30 12:25:32 +0200

    Aggiornamento esericizi

--- src/com/company/esercizio5/es5.java
+++ src/com/company/esercizio5/es5.java
@@ -1,20 +1,27 @@
 package com.company.esercizio5;
 
-
 import java.util.Scanner;
 
 public class es5 {
+
     public static void main (String [] args) {
+
         Scanner input = new Scanner(System.in);
-        //stampa valori da o a 10 e i loro rispettivi quadrati e cubo
+
+        //stampa valori da o a 10 e il loro rispettivo quadrato e cubo
+
         System.out.println("NUMBER SQUARE CUBE");
+
         for( int i = 0; i < 11; i++){
             System.out.printf("%d   %d   %d\n", i, i*i, i*i*i);
         }
+
         //inserimento 5 numeri e controllo zero, positivi e negativi
+
         int number, numPos = 0, numNeg = 0, numZero = 0;
+
         for(int i = 0; i < 5; i++){
-            System.out.printf("Enter the %d",i+1);
+            System.out.printf("Enter the %d°: ",i+1);
             number = input.nextInt();
             if(number == 0){
                 numZero++;
@@ -25,5 +32,21 @@ public class es5 {
             }
         }
         System.out.printf("Number zero: %d\nNumber positive: %d\nNumber negative: %d", numZero, numPos, numNeg);
+
+        //calcolatore indice massa corporea
+
+        System.out.print("Enter your weight: ");
+        int weight = input.nextInt();
+        System.out.print("Enter your height: ");
+        float height = input.nextFloat();
+        float BMI = weight/(height*height);
+        System.out.printf("\nYour BMI is: %f", BMI);
+        System.out.println("\n");
+        System.out.println("BMI Categories: \n" +
+                "Underweight = <18.5\n" +
+                "Normal weight = 18.5–24.9 \n" +
+                "Overweight = 25–29.9 \n" +
+                "Obesity = BMI of 30 or greater");
+        input.close();
     }
 }
